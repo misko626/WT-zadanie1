af73724cc6479c06bc4bae6e128203ae5eda510b
MichalPopovic <popovic.michal626@gmail.com>
2022-11-11 20:24:34 +0100

    init commit

---
+++ Zadanie 1/main_script.js
@@ -0,0 +1,59 @@
+/*
+ * NAV Mobile
+ */
+const navMobileIcon = document.querySelector(".menu-icon");
+let isMenuOpen = false;
+function onClickMenu() {
+  navContainer.classList.toggle("nav-container-mobile");
+  navContainer.classList.toggle("nav-container");
+  if (!isMenuOpen) {
+    navMobileIcon.src = "./assets/icons8-close.svg";
+  } else {
+    navMobileIcon.src = "./assets/icons8-menu.svg";
+  }
+
+  isMenuOpen = isMenuOpen === false ? true : false;
+  console.log(isMenuOpen);
+}
+
+/*
+ * NAV Animation from TOP
+ */
+
+const navContainer = document.getElementById("navContainer");
+const navbar = document.querySelector(".nav-bar");
+const navLinks = document.querySelectorAll(".nav-link");
+
+const navLinkSearch = document.querySelector(".nav-link-search");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 50) {
+    navContainer.classList.add("nav-contaniner-scrolled");
+    for (var link of navLinks) {
+      link.classList.add("link-scrolled");
+    }
+  } else {
+    navContainer.classList.remove("nav-contaniner-scrolled");
+    for (var link of navLinks) {
+      link.classList.remove("link-scrolled");
+    }
+  }
+});
+
+/*
+ * SCROLL DOWN AND ANIMATE
+ */
+
+window.addEventListener("scroll", appearElement);
+
+function appearElement() {
+  //screen sroll value
+  const screenPosition = (window.innerHeight / 2) * 1.3;
+  const servicesLogo = document.querySelector(".services-logo");
+
+  const logoPosition = servicesLogo.getBoundingClientRect().top;
+
+  if (logoPosition < screenPosition) {
+    servicesLogo.classList.add("appear-logo");
+  }
+}
